4baff864886ebc1c29dfe45206e5b67f92f59863
Bemnet77 <bemnetretta7@gmail.com>
2022-06-06 07:00:40 -0700

    .

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,13 @@
  * main - checks numbers
  * Return: 0
  */
-
 int main(void)
 {
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar (ch);
 for (ch = 'A'; ch <= 'Z'; ch++)
-{                                                                                                           
+{
 putchar (ch);
 }
 putchar ('\n');
