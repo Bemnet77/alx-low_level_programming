703cb748615275ff911eb9bfce84e5e73dc58fd1
Bemnet77 <bemnetretta7@gmail.com>
2022-06-06 06:54:57 -0700

    .

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - checks numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar (ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+{                                                                                                           
+putchar (ch);
+}
+putchar ('\n');
+return (0);
+}
