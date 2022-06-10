cc888a06c7c5911f1e51a8b274cf5169b1041074
Bemnet77 <bemnetretta7@gmail.com>
2022-06-10 09:52:44 -0700

    ///

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - checks
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
