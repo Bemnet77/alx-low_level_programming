9a68fb7af3f2a2cf4106d9e53dd1771a5c6148ac
Bemnet77 <bemnetretta7@gmail.com>
2022-06-10 12:50:11 -0700

    \]"
    git push
    vi 6-print_line.c
    git commit -m ]"

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_line - file
+ * @n: n
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	for (x = n; x > 0; x--)
+		putchar('_');
+	printf("\n");
+}
