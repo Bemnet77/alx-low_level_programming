43482ed1a5cba0cd9e924cf8c1d6d01b37b6ca42
Bemnet77 <bemnetretta7@gmail.com>
2022-06-10 03:12:45 -0700

    ily betty

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isdigit - check numbers
+ * @c: variable
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+		return (0);
+}
