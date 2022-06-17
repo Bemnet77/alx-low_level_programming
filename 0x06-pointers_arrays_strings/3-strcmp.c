727b3030941d83fc45dc05e4ce91885c352b6374
Bemnet77 <bemnetretta7@gmail.com>
2022-06-17 09:56:36 -0700

    ...

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int a = 0, b = 0, c = 0, r = 0, lim;
+
 	while (s1[a])
 	{
 		a++;
@@ -28,13 +29,17 @@ int _strcmp(char *s1, char *s2)
 	while (c <= lim)
 	{
 		if (s1[c] == s2[c])
-		{										c++;										continue;
+		{
+			c++;										continue;
 		}
-		else										{										r = s1[c] - s2[c];
-			break;										}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
 		c++;
 	}
-	
+
 	return (r);
 
 }
